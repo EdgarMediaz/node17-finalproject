206ba4337a203b73f0de176336855a0264915418
EdgarMediaz <102551598+EdgarMediaz@users.noreply.github.com>
2022-12-03 11:11:20 -0800

    port added to server

--- utils/database.util.js
+++ utils/database.util.js
@@ -9,6 +9,7 @@ const db = new Sequelize({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   database: process.env.DB,
   logging: false,
   dialectOptions:
